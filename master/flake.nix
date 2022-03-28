116d629d52aff71604df7f7d6d2c0c6fa7320e47
hugosenari <hugosenari@gmail.com>
2022-03-27 21:48:28 -0300

    chore: re index chain

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-chain-master.ref   = "refs/heads/master";
   inputs.src-chain-master.owner = "khchen";
   inputs.src-chain-master.repo  = "chain";
-  inputs.src-chain-master.dir   = "";
   inputs.src-chain-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
